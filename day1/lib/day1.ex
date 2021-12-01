222fdcc6dc2927039bd573cd9f9777baf162dc9c
Stephen Snow <stephen.c.snow@gmail.com>
2021-12-01 11:58:13 -0500

    Day 1, part 2 (refactored part 1)

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -5,6 +5,7 @@ defmodule Day1 do
     |> Enum.map(&String.to_integer/1)
   end
 
+  # part 1
   def count_increases(report) do
     [first_depth | rest] = report
     {total_increases, _} = Enum.reduce(rest, {0, first_depth}, fn depth, {num_increases, prev_depth} ->
@@ -16,8 +17,28 @@ defmodule Day1 do
     total_increases
   end
 
-  def main do
+  # part 2
+  def count_increases_sliding_window(report, window_size) do
+    {init_window, rest} = Enum.split(report, window_size)
+
+    {total_increases, _} = Enum.reduce(rest, {0, init_window}, fn depth, {num_increases, prev_window} ->
+      [_ | remaining] = prev_window
+      new_window = remaining ++ [depth]
+      cond do
+        Enum.sum(new_window) > Enum.sum(prev_window) -> {num_increases + 1, new_window}
+        true -> {num_increases, new_window}
+      end
+    end)
+    total_increases
+  end
+
+  def part1 do
     parse_input("input.txt")
-    |> count_increases
+    |> count_increases_sliding_window(1)
+  end
+
+  def part2 do
+    parse_input("input.txt")
+    |> count_increases_sliding_window(3)
   end
 end
