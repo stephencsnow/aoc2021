45f13aded8fdf2748646d14e52429d1813196479
Stephen Snow <stephen.c.snow@gmail.com>
2021-12-01 12:15:35 -0500

    Day 1 tests (should have done first)

--- day1/test/day1_test.exs
+++ day1/test/day1_test.exs
@@ -2,7 +2,41 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  test "greets the world" do
-    assert Day1.hello() == :world
+  test "sliding window of 1" do
+    txt = "199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263"
+    data = txt
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&String.to_integer/1)
+
+    assert Day1.count_increases_sliding_window(data, 1) == 7
+  end
+
+  test "sliding window of 3" do
+    txt = "199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263"
+    data = txt
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&String.to_integer/1)
+
+    assert Day1.count_increases_sliding_window(data, 3) == 5
   end
 end
