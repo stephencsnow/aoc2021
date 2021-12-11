5096ff1dc97090007010c273ec80d2ae1e1ea2da
Stephen Snow <stephen.c.snow@gmail.com>
2021-12-11 18:57:46 -0500

    day2 part2

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -12,10 +12,24 @@ defmodule Day2 do
   def get_xy([{:down, amount} | rest], {x, y}), do: get_xy(rest, {x, y + amount})
   def get_xy([{:up, amount} | rest], {x, y}), do: get_xy(rest, {x, y - amount})
 
+  def get_xy(movements, current_position, aim)
+  def get_xy([], {x, y}, _), do: {x, y}
+  def get_xy([{:forward, amount} | rest], {x, y}, aim), do: get_xy(rest, {x + amount, y + amount * aim}, aim)
+  def get_xy([{:down, amount} | rest], {x, y}, aim), do: get_xy(rest, {x, y}, aim + amount)
+  def get_xy([{:up, amount} | rest], {x, y}, aim), do: get_xy(rest, {x, y}, aim - amount)
+
   def part1 do
-    {x, y}=
+    {x, y} =
       parse_input("input.txt")
       |> get_xy({0, 0})
+
+    x * y
+  end
+
+  def part2 do
+    {x, y} =
+      parse_input("input.txt")
+      |> get_xy({0, 0}, 0)
 
     x * y
   end
--- day2/test/day2_test.exs
+++ day2/test/day2_test.exs
@@ -2,7 +2,7 @@ defmodule Day2Test do
   use ExUnit.Case
   doctest Day2
 
-  test "greets the world" do
+  test "part 1" do
     txt = "
     forward 5
     down 5
@@ -17,5 +17,22 @@ defmodule Day2Test do
     |> Enum.map(fn [dir, amount] -> {String.to_atom(dir), String.to_integer(amount)} end)
 
     assert Day2.get_xy(data, {0, 0}) == {15, 10}
+  end
+
+  test "part 2" do
+    txt = "
+    forward 5
+    down 5
+    forward 8
+    up 3
+    down 8
+    forward 2"
+    data = txt
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.split/1)
+    |> Enum.map(fn [dir, amount] -> {String.to_atom(dir), String.to_integer(amount)} end)
+
+    assert Day2.get_xy(data, {0, 0}, 0) == {15, 60}
   end
 end
