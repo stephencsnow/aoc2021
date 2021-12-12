91ca1c127fe0d7376cb58ffefd20fa47b9f536fc
Stephen Snow <stephen.c.snow@gmail.com>
2021-12-11 20:02:37 -0500

    day3 part1

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -1,18 +1,51 @@
 defmodule Day3 do
-  @moduledoc """
-  Documentation for `Day3`.
-  """
+  def parse_input(filename) do
+    File.read!(filename)
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> String.split(line, "", trim: true) |> Enum.map(&String.to_integer/1) end)
+  end
 
-  @doc """
-  Hello world.
+  defp transpose(list_of_lists) do
+    list_of_lists
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
+  end
 
-  ## Examples
+  defp times_one_appears(list), do: Enum.sum(list)
+  defp one_is_most_common?(list), do: times_one_appears(list) > Enum.count(list) / 2
 
-      iex> Day3.hello()
-      :world
+  def most_common_bit(list) do
+    case one_is_most_common?(list) do
+      true -> 1
+      false -> 0
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def least_common_bit(list) do
+    case one_is_most_common?(list) do
+      true -> 0
+      false -> 1
+    end
+  end
+
+  def gamma(list_of_lists) do
+    list_of_lists
+    |> transpose
+    |> Enum.map(&most_common_bit/1)
+    |> Integer.undigits(2)
+  end
+
+  def epsilon(list_of_lists) do
+    list_of_lists
+    |> transpose
+    |> Enum.map(&least_common_bit/1)
+    |> Integer.undigits(2)
+  end
+
+  def power_consumption(list_of_lists), do: gamma(list_of_lists) * epsilon(list_of_lists)
+
+  def part1 do
+    parse_input("input.txt")
+    |> power_consumption
   end
 end
--- day3/test/day3_test.exs
+++ day3/test/day3_test.exs
@@ -2,7 +2,28 @@ defmodule Day3Test do
   use ExUnit.Case
   doctest Day3
 
-  test "greets the world" do
-    assert Day3.hello() == :world
+  test "part 1" do
+    txt = "
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010"
+
+    data = txt
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(fn line -> String.split(line, "", trim: true) |> Enum.map(&String.to_integer/1) end)
+
+    assert Day3.gamma(data) == 22
+    assert Day3.epsilon(data) == 9
+    assert Day3.power_consumption(data) == 198
   end
 end
