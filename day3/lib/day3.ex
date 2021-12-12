771621eee00af7b2a4473ae66d2bc95f410fb15f
Stephen Snow <stephen.c.snow@gmail.com>
2021-12-11 20:42:06 -0500

    day3 part2

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -12,40 +12,84 @@ defmodule Day3 do
   end
 
   defp times_one_appears(list), do: Enum.sum(list)
-  defp one_is_most_common?(list), do: times_one_appears(list) > Enum.count(list) / 2
 
-  def most_common_bit(list) do
-    case one_is_most_common?(list) do
-      true -> 1
-      false -> 0
-    end
-  end
-
-  def least_common_bit(list) do
-    case one_is_most_common?(list) do
+  def most_common_bit(list, tiebreak \\ 1) do
+    len = Enum.count(list)
+    ones = times_one_appears(list)
+    cond do
+      ones > len / 2 -> 1
+      ones == len / 2 -> tiebreak
       true -> 0
-      false -> 1
     end
   end
 
-  def gamma(list_of_lists) do
+  def least_common_bit(list, tiebreak \\ 0) do
+    len = Enum.count(list)
+    ones = times_one_appears(list)
+    cond do
+      ones > len / 2 -> 0
+      ones == len / 2 -> tiebreak
+      true -> 1
+    end
+  end
+
+  def st_common_bit(list, :most), do: most_common_bit(list)
+  def st_common_bit(list, :least), do: least_common_bit(list)
+
+  defp only_st_bits(list_of_lists, st) do
     list_of_lists
     |> transpose
-    |> Enum.map(&most_common_bit/1)
+    |> Enum.map(fn list -> st_common_bit(list, st) end)
     |> Integer.undigits(2)
   end
 
-  def epsilon(list_of_lists) do
-    list_of_lists
-    |> transpose
-    |> Enum.map(&least_common_bit/1)
-    |> Integer.undigits(2)
-  end
+  def gamma(list_of_lists), do: only_st_bits(list_of_lists, :most)
+
+  def epsilon(list_of_lists), do: only_st_bits(list_of_lists, :least)
+
 
   def power_consumption(list_of_lists), do: gamma(list_of_lists) * epsilon(list_of_lists)
+
+  defp recurse(list_of_lists, st_bit, leading_bits \\ [])
+
+  defp recurse([last_value], _, leading_bits) do
+    leading_bits ++ last_value
+    |> Integer.undigits(2)
+  end
+
+  defp recurse(list_of_lists, st_bit, leading_bits) do
+    desired_bit =
+      list_of_lists
+      |> transpose
+      |> List.first
+      |> st_common_bit(st_bit)
+
+    remaining_elements =
+      list_of_lists
+      |> Enum.map(fn list -> List.pop_at(list, 0) end)
+      |> Enum.filter(fn {bit, _} -> bit == desired_bit end)
+      |> Enum.map(fn {_, remaining} -> remaining end)
+
+    recurse(remaining_elements, st_bit, leading_bits ++ [desired_bit])
+  end
+
+  def oxygen_generator_rating(list_of_lists) do
+    recurse(list_of_lists, :most)
+  end
+
+  def co2_scrubber_rating(list_of_lists) do
+    recurse(list_of_lists, :least)
+  end
+
+  def life_support_rating(list_of_lists), do: oxygen_generator_rating(list_of_lists) * co2_scrubber_rating(list_of_lists)
 
   def part1 do
     parse_input("input.txt")
     |> power_consumption
+  end
+
+  def part2 do
+    parse_input("input.txt")
+    |> life_support_rating
   end
 end
--- day3/test/day3_test.exs
+++ day3/test/day3_test.exs
@@ -26,4 +26,29 @@ defmodule Day3Test do
     assert Day3.epsilon(data) == 9
     assert Day3.power_consumption(data) == 198
   end
+
+  test "part 2" do
+    txt = "
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010"
+
+    data = txt
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(fn line -> String.split(line, "", trim: true) |> Enum.map(&String.to_integer/1) end)
+
+    assert Day3.oxygen_generator_rating(data) == 23
+    assert Day3.co2_scrubber_rating(data) == 10
+    assert Day3.life_support_rating(data) == 230
+  end
 end
