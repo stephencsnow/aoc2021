e6dfbe3ea78156b3055d09e93b5b9c311b3c615e
Stephen Snow <stephen.c.snow@gmail.com>
2021-12-14 18:54:08 -0500

    day7 part2

--- day7/crabs.py
+++ day7/crabs.py
@@ -2,6 +2,7 @@ from typing import Callable
 
 Position = int
 Fuel = int
+FuelFunction = Callable[[list[Position], Position], Fuel]
 
 
 def fuel_cost(positions: list[Position], position_to_move_to: Position) -> Fuel:
@@ -12,10 +13,26 @@ def fuel_cost(positions: list[Position], position_to_move_to: Position) -> Fuel:
     return total_fuel
 
 
-def get_fuel_costs(positions: list[Position]) -> dict[Position, Fuel]:
+def series_sum(n: int) -> int:
+    """sum of 1 + 2 + 3 + ... + N"""
+    return int(n * (n + 1) / 2)
+
+
+def dynamic_fuel_cost(positions: list[Position], position_to_move_to: Position) -> Fuel:
+    total_fuel = 0
+    for position in positions:
+        distance = abs(position - position_to_move_to)
+        total_fuel += series_sum(distance)
+
+    return total_fuel
+
+
+def get_fuel_costs(
+    positions: list[Position], function: FuelFunction
+) -> dict[Position, Fuel]:
     costs = {}
     for position in range(min(positions), max(positions) + 1):
-        costs[position] = fuel_cost(positions, position)
+        costs[position] = function(positions, position)
 
     return costs
 
@@ -23,11 +40,12 @@ def get_fuel_costs(positions: list[Position]) -> dict[Position, Fuel]:
 def gradient_descent(
     positions: list[Position],
     current_position: Position,
-    function: Callable[[list[Position], Position], Fuel],
+    function: FuelFunction,
 ) -> Position:
     one_below = function(positions, current_position - 1)
     current_value = function(positions, current_position)
     one_above = function(positions, current_position + 1)
+    print(one_above, current_value, one_below, current_position)
 
     match one_below, current_value, one_above:
         case _, current_value, _ if one_below > current_value < one_above:
@@ -54,10 +72,27 @@ def part1(positions: list[Position]) -> int:
     return fuel_cost(positions, optimal_position)
 
 
+def part2(positions: list[Position]) -> int:
+    """
+    Gradient descent approach here.
+    Start with the average value, then move in the direction of fuel cost being lower.
+
+    Assumes that local minima == local maxima
+
+    LOL turns out for my input, the mean is the correct answer? wild.
+    """
+    # start with mean
+    initial_position = int(sum(positions) / len(positions))
+    optimal_position = gradient_descent(positions, initial_position, dynamic_fuel_cost)
+
+    return dynamic_fuel_cost(positions, optimal_position)
+
+
 def main():
     with open("input.txt") as f:
         positions = [int(x) for x in f.read().split(",")]
     print("Part 1", part1(positions))
+    print("Part 2", part2(positions))
 
 
 if __name__ == "__main__":
--- day7/test_crabs.py
+++ day7/test_crabs.py
@@ -1,4 +1,4 @@
-from day7.crabs import part1
+from day7.crabs import part1, part2
 
 
 def fixture() -> list[int]:
@@ -7,3 +7,7 @@ def fixture() -> list[int]:
 
 def test_part1():
     assert part1(fixture()) == 37
+
+
+def test_part2():
+    assert part2(fixture()) == 168
