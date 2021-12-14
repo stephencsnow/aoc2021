db23ea5f78eec3155b637586d490bbeace569eb9
Stephen Snow <stephen.c.snow@gmail.com>
2021-12-14 18:19:47 -0500

    day6 part 1

---
+++ day6/lanterns.py
@@ -0,0 +1,42 @@
+from collections import Counter
+
+Timer = int
+
+TIME_TO_CREATE = 0
+RESET_TIMER = 6
+INITIAL_TIMER = 8
+
+
+def parse_data(text: str) -> list[Timer]:
+    return [int(timer) for timer in text.split(",")]
+
+
+def part1(timers: list[Timer]) -> int:
+    counter = Counter(timers)
+
+    steps = 80
+
+    for step in range(steps):
+        new_counter = Counter()
+        for timer, num_fish in counter.items():
+            if timer == TIME_TO_CREATE:
+                # fish getting ready to create again
+                new_counter[RESET_TIMER] += num_fish
+                # new fish being born
+                new_counter[INITIAL_TIMER] += num_fish
+            else:
+                new_counter[timer - 1] += num_fish
+
+        counter = new_counter
+
+    return sum(counter.values())
+
+
+def main():
+    with open("input.txt") as f:
+        timers = parse_data(f.read())
+    print(part1(timers))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day6/test_lanterns.py
@@ -0,0 +1,5 @@
+from day6.lanterns import part1
+
+
+def test_part1():
+    assert part1([3, 4, 3, 1, 2]) == 5934
