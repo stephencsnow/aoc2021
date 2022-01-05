efc1663f26cd4433d26e7ea02fd49e74a593d973
Stephen Snow <stephen.c.snow@gmail.com>
2022-01-05 11:01:17 -0500

    day10 part2

--- day10/syntax.py
+++ day10/syntax.py
@@ -1,4 +1,5 @@
 from collections import deque
+from statistics import median
 
 OPENING_CHARACTERS = ("(", "[", "{", "<")
 CLOSING_CHARACTERS = (")", "]", "}", ">")
@@ -10,11 +11,18 @@ CHARACTER_PAIRS = {
     "<": ">",
 }
 
-SCORES = {
+CORRUPTION_SCORES = {
     ")": 3,
     "]": 57,
     "}": 1197,
     ">": 25137,
+}
+
+COMPLETION_SCORES = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
 }
 
 
@@ -51,14 +59,54 @@ def part1(lines: list[str]) -> int:
             # line is corrupted
             illegal_characters.append(first_illegal_character)
 
-    return sum(SCORES[char] for char in illegal_characters)
+    return sum(CORRUPTION_SCORES[char] for char in illegal_characters)
+
+
+def get_completion_string(line: str) -> str | None:
+    """Get completion string if line is not corrupted.
+
+    If line is corrupted, return none.
+    """
+    # stack based solution
+    # we store closing characters on a stack
+    stack = []
+    for character in line:
+        if character in OPENING_CHARACTERS:
+            stack.append(CHARACTER_PAIRS[character])
+        elif not stack:
+            return None
+        else:
+            expected_character = stack.pop()
+            if character != expected_character:
+                return None
+
+    return ''.join(reversed(stack))
+
+
+def score_completion_string(completion_string: str) -> int:
+    score = 0
+    for character in completion_string:
+        score *= 5
+        score += COMPLETION_SCORES[character]
+    return score
+
+
+def part2(lines: list[str]) -> int:
+    completion_strings = []
+    for line in lines:
+        if completion_string := get_completion_string(line):
+            completion_strings.append(completion_string)
+
+    scores = [score_completion_string(cs) for cs in completion_strings]
+
+    return median(sorted(scores))
 
 
 def main():
     with open("input.txt") as f:
         data = f.read().splitlines()
-    print("Part 1: ", part1(data))
-    # print("Part 2: ", part2(data))
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
 
 
 if __name__ == "__main__":
--- day10/test_syntax.py
+++ day10/test_syntax.py
@@ -1,4 +1,4 @@
-from day10.syntax import part1, get_first_illegal_character
+from day10.syntax import part1, get_first_illegal_character, part2, get_completion_string
 
 
 def fixture() -> list[str]:
@@ -23,3 +23,11 @@ def test_get_first_illegal_character():
 
 def test_part1():
     assert part1(fixture()) == 26397
+
+
+def test_get_completion_string():
+    assert get_completion_string("[({(<(())[]>[[{[]{<()<>>") == "}}]])})]"
+    assert get_completion_string("[(()[<>])]({[<{<<[]>>(") == ")}>]})"
+
+def test_part2():
+    assert part2(fixture()) == 288957
